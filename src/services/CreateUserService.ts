db4e7672680b8415d2a76080c13ab1500f021255
Matheus F Martins <martinsmf00@gmail.com>
2021-06-23 00:59:28 -0300

    .
    .

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm'
 import { UserRepositories } from '../repositories/UserRepositories'
+import { Err } from '../err'
 
 interface IUserRequest {
   name: string
@@ -12,13 +13,13 @@ class CreateUserService {
     const userRepository = getCustomRepository(UserRepositories)
 
     if (!email) {
-      throw new Error('Email incorrect')
+      throw new Err(401, 'Email incorrect')
     }
 
     const userAlreadyExists = await userRepository.findOne({ email })
 
     if (userAlreadyExists) {
-      throw new Error('User already exists')
+      throw new Err(401, 'User already exists')
     }
 
     const user = userRepository.create({
